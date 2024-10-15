d3d77422a246c4d0c77ce283dc2f7b1abef0e364
Ricardo Rocha <ricardomjr2@gmail.com>
2024-10-14 21:06:28 -0300

    criado a tela de cadstro

--- script.js
+++ script.js
@@ -1,19 +1,2 @@
 const tabela = document.querySelector('table')
 const dados = document.querySelectorAll('td')
-
-dados[0].innerText = "roberto"
-
-dados.forEach(dado => {
-    console.log(dado.innerText)
-})
-
-const adicionarLinha = () => {
-   let novaLinha =  tabela.insertRow()
-
-   for (let i=0;i<dados.length;i++) {
-    let novaCelula = novaLinha.insertCell()
-    novaCelula.textContent = dados[i].innerText
-   }
-}
-
-adicionarLinha()
