c6cda5c3b17a3449a4a5d020c85e95c5a1a84982
Ricardo Rocha <ricardomjr2@gmail.com>
2024-10-17 22:11:15 -0300

    script e style modificados

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 
-const buttonGravar = document.querySelector('.gravar')
+const buttonGravar = document.querySelector('.gravarC')
+const bCliente = document.querySelector('.bCliente')
+const bProduto = document.querySelector('.bProduto')
+const telaCadastro = document.querySelector('.telaCadastro')
+const telaCadastroProduto = document.querySelector('.telaCadastroProduto')
+const msg = document.querySelector('.msg')
 
+bCliente.addEventListener('click', () => {
+    telaCadastro.style.display = 'block'
+    telaCadastroProduto.style.display = 'none'
+})
+
+bProduto.addEventListener('click', () => {
+    telaCadastroProduto.style.display = 'block'
+    telaCadastro.style.display = 'none'
+})
 
 let cliente = []
 
 buttonGravar.addEventListener('click', (event) => {
 
-    const email = document.querySelector('.email').value
-    const tel = document.querySelector('.tel').value
-    const nome = document.querySelector('.nome').value
+  
+    let email = document.querySelector('.email').value
+    let tel = document.querySelector('.tel').value
+    let nome = document.querySelector('.nome').value
+
+    if (nome == '') {
+        msg.style.display = 'block'
+        msg.innerHTML = 'Por favor preencha o nome'
+        return
+    }
+
+     msg.style.display = 'none'
 
     novoCliente = {
         nome,
@@ -19,6 +42,14 @@ buttonGravar.addEventListener('click', (event) => {
     cliente.push(novoCliente)
     console.log(cliente)
 
- 
+    limpar()
+   
 })
 
+
+const limpar = () => {
+    document.querySelector('.email').value = ""
+    document.querySelector('.tel').value = ""
+    document.querySelector('.nome').value = ""
+    
+}
