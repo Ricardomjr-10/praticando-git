7cc2e69ed1b2b6eefdafe7a835b90f235de1e49e
Ricardo Rocha <ricardomjr2@gmail.com>
2024-10-19 10:29:24 -0300

    criando tabela dinamica

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 
 const buttonGravar = document.querySelector('.gravarC')
+const buttonGravarP = document.querySelector('.gravarP')
 const bCliente = document.querySelector('.bCliente')
 const bProduto = document.querySelector('.bProduto')
 const telaCadastro = document.querySelector('.telaCadastro')
 const telaCadastroProduto = document.querySelector('.telaCadastroProduto')
 const msg = document.querySelector('.msg')
+const tabela = document.querySelector('.tabela')
 
 bCliente.addEventListener('click', () => {
     telaCadastro.style.display = 'block'
     telaCadastroProduto.style.display = 'none'
+    msg.style.display = 'none'
 })
 
 bProduto.addEventListener('click', () => {
     telaCadastroProduto.style.display = 'block'
     telaCadastro.style.display = 'none'
+    msg.style.display = 'none'
 })
 
-let cliente = []
+let clientes = []
 
 buttonGravar.addEventListener('click', (event) => {
 
@@ -39,17 +43,58 @@ buttonGravar.addEventListener('click', (event) => {
         tel
     }
 
-    cliente.push(novoCliente)
-    console.log(cliente)
+    clientes.push(novoCliente)
+    console.log(clientes)
 
-    limpar()
+    limpar('.email', '.nome', '.tel')
+
+    tabela.innerHTML = `<table>
+            <tr>
+                <th>nome</th>
+                <th>email</th>
+                <th>Telefone</th>
+            </tr>
+            <tr>
+                <td>${clientes[0].nome}</td>
+                <td>${clientes[0].email}</td>
+                <td>${clientes[0].tel}</td>
+            </tr>
+        </table>`
    
 })
 
+let produtos = []
 
-const limpar = () => {
-    document.querySelector('.email').value = ""
-    document.querySelector('.tel').value = ""
-    document.querySelector('.nome').value = ""
+buttonGravarP.addEventListener('click', () => {
+    let produto = document.querySelector('.produto').value
+    let quantidade = document.querySelector('.quantidade').value
+
+    if (produto == '') {
+        msg.style.display = 'block'
+        msg.innerHTML = 'Por favor digite o produto'
+        return
+    }
+
+    msg.style.display = 'none'
+
+    novoProduto = {
+        produto,
+        quantidade
+    }
+
+    produtos.push(novoProduto)
+    console.log(produtos)
+
+    limpar('.produto', '.quantidade', '.quantidade')
+    
+})
+
+
+
+
+const limpar = (nome1, nome2 ,nome3) => {
+    document.querySelector(nome1).value = ""
+    document.querySelector(nome2).value = ""
+    document.querySelector(nome3).value = ""
     
 }
